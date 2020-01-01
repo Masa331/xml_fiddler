d3cd02437625827e22a52d4a80419ea57db6136e
Premysl Donat <pdonat@seznam.cz>
2020-01-01 20:35:02 +0100

    wi

---
+++ scratchpad.js
@@ -0,0 +1,22 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+var text, parser, xmlDoc;
+
+text = "<bookstore><book>" +
+"<title>Everyday Italian</title>" +
+"<author>Giada De Laurentiis</author>" +
+"<year>2005</year>" +
+"</book></bookstore>";
+
+parser = new DOMParser();
+xmlDoc = parser.parseFromString(text,"text/xml");
+console.log(xmlDoc);
+//document.getElementById("demo").innerHTML =
+//xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
